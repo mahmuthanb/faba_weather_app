cd522d295188c344a6b5a65b31b8bbb11e113eef
Mahmuthan <mahmuthanb@gmail.com>
2025-04-18 00:04:00 +0300

    bundleId changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.faba_weather_app"
+    namespace = "com.fabaint.weather.android"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.faba_weather_app"
+        applicationId = "com.fabaint.weather.android"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
