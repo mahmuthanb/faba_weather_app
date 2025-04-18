77ece6133313870e650615b06edad08ea438337b
Mahmuthan <mahmuthanb@gmail.com>
2025-04-18 09:46:00 +0300

    app moved to subfolder and replaced with bootstrap zone

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,6 @@
-import 'package:faba_weather_app/core/di/locator.dart';
-import 'package:flutter/material.dart';
-import 'package:faba_weather_app/config/theme.dart';
-import 'package:faba_weather_app/config/router/app_router.dart';
-import 'package:faba_weather_app/core/l10n/app_localizations.dart';
-import 'package:faba_weather_app/data/services/storage_services.dart';
-import 'package:faba_weather_app/presentation/providers/language_provider.dart';
-import 'package:faba_weather_app/presentation/providers/theme_provider.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:provider/provider.dart';
+import 'package:faba_weather_app/core/bootstrap/bootstrap.dart';
+import 'package:faba_weather_app/presentation/app.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await StorageService.init();
-  await setupDI("dev");
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => ThemeProvider()),
-        ChangeNotifierProvider(create: (context) => LanguageProvider()),
-      ],
-      child: const MyApp(),
-    ),
-  );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: appRouter,
-      title: 'FABA Weather App',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: context.watch<ThemeProvider>().themeMode,
-      locale: context.watch<LanguageProvider>().locale,
-      supportedLocales: LanguageProvider.supportedLocales,
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-    );
-  }
+  bootstrap(() => App(), "dev");
 }
---
+++ lib/presentation/app.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:faba_weather_app/config/theme.dart';
+import 'package:faba_weather_app/config/router/app_router.dart';
+import 'package:faba_weather_app/core/l10n/app_localizations.dart';
+import 'package:faba_weather_app/presentation/providers/language_provider.dart';
+import 'package:faba_weather_app/presentation/providers/theme_provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+        ChangeNotifierProvider(create: (context) => LanguageProvider()),
+      ],
+      child: const MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      title: 'FABA Weather App',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: context.watch<ThemeProvider>().themeMode,
+      locale: context.watch<LanguageProvider>().locale,
+      supportedLocales: LanguageProvider.supportedLocales,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+    );
+  }
+}
