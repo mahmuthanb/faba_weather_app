e932f159551fb90a275659f0546573c92d40920c
Mahmuthan <mahmuthanb@gmail.com>
2025-04-18 09:40:24 +0300

    bootstrapping app with error logger

---
+++ lib/core/bootstrap/bootstrap.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+import 'dart:developer';
+import 'package:faba_weather_app/core/di/locator.dart';
+import 'package:faba_weather_app/core/logger/logger.dart';
+import 'package:faba_weather_app/data/services/storage_services.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+Future<void> bootstrap(FutureOr<Widget> Function() builder, String env) async {
+  await runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await StorageService.init();
+      FlutterError.onError = (details) {
+        log(details.exceptionAsString(), stackTrace: details.stack);
+        FlutterError.dumpErrorToConsole(details);
+      };
+      SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+      await setupDI(env);
+      runApp(await builder());
+    },
+    (error, stackTrace) {
+      Log.e(error.toString(), tag: "Zone Error");
+      Log.e(stackTrace.toString(), tag: "Zone StackTrace");
+    },
+  );
+}
