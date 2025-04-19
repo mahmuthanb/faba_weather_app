3e80687c71c2782e5f6f967be7c84b5036a75cea
Mahmuthan <mahmuthanb@gmail.com>
2025-04-19 21:13:47 +0300

    Create time_utils.dart

---
+++ lib/core/utils/time_utils.dart
@@ -0,0 +1,6 @@
+class TimeUtils {
+  static String formatTime(int timestamp) {
+    final date = DateTime.fromMillisecondsSinceEpoch(timestamp * 1000);
+    return '${date.hour.toString().padLeft(2, '0')}:${date.minute.toString().padLeft(2, '0')}';
+  }
+}
