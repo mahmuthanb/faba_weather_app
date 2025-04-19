3438cebbc07d79a91909a03ab043accff6d1069d
Mahmuthan <mahmuthanb@gmail.com>
2025-04-19 15:54:45 +0300

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:faba_weather_app/main.dart';
+import 'package:faba_weather_app/presentation/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
