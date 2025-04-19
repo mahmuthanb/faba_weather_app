a0eeff06dd76ddade1d027957c0eba5a9b7833df
Mahmuthan <mahmuthanb@gmail.com>
2025-04-19 21:12:12 +0300

    weather forecast card

---
+++ lib/presentation/widgets/weather_forecast_card.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:faba_weather_app/domain/entities/weather.dart';
+import 'package:intl/intl.dart';
+import 'package:faba_weather_app/data/models/forecast_response_model.dart';
+
+class WeatherForecastCard extends StatelessWidget {
+  final Weather weather;
+  final CityInfo? cityInfo;
+
+  const WeatherForecastCard({super.key, required this.weather, this.cityInfo});
+
+  @override
+  Widget build(BuildContext context) {
+    final time = DateTime.fromMillisecondsSinceEpoch(weather.timestamp * 1000);
+    final formattedTime = DateFormat('HH:mm').format(time);
+
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Colors.white.withOpacity(0.2), width: 1),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            formattedTime,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            '${weather.temperature.round()}°',
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            weather.weatherCondition,
+            style: TextStyle(
+              color: Colors.white.withOpacity(0.8),
+              fontSize: 14,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
