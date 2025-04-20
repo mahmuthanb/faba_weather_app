133e4cfb0b3450e08d40e49771142ef5c6c34c2e
Mahmuthan <mahmuthanb@gmail.com>
2025-04-21 01:26:57 +0300

    Create mock_theme_provider.dart

---
+++ test/mocks/mock_theme_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MockThemeProvider extends ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.system;
+
+  ThemeMode get themeMode => _themeMode;
+
+  void setThemeMode(ThemeMode mode) {
+    if (_themeMode != mode) {
+      _themeMode = mode;
+      notifyListeners();
+    }
+  }
+
+  bool get isDarkMode => _themeMode == ThemeMode.dark;
+}
