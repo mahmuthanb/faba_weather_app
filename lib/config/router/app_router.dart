35c0ec7fc31fb6ebf6c37562944c9e19758d7c42
Mahmuthan <mahmuthanb@gmail.com>
2025-04-20 21:27:48 +0300

    fadeIn transition to pageBuilder

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:faba_weather_app/domain/entities/weather.dart';
 import 'package:faba_weather_app/presentation/screens/weather_detail/weather_detail_page.dart';
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:faba_weather_app/config/router/routes.dart';
 import 'package:faba_weather_app/presentation/screens/settings/settings_page.dart';
@@ -15,14 +16,26 @@ final GoRouter appRouter = GoRouter(
     GoRoute(
       path: AppRoutes.settings,
       name: 'settings',
-      builder: (context, state) => const SettingsPage(),
+      pageBuilder:
+          (context, state) => CustomTransitionPage<void>(
+            key: state.pageKey,
+            child: const SettingsPage(),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) =>
+                    FadeTransition(opacity: animation, child: child),
+          ),
     ),
     GoRoute(
       path: AppRoutes.weatherDetail,
       name: 'weatherDetail',
-      builder:
-          (context, state) =>
-              WeatherDetailPage(weather: state.extra as Weather),
+      pageBuilder:
+          (context, state) => CustomTransitionPage<void>(
+            key: state.pageKey,
+            child: WeatherDetailPage(weather: state.extra as Weather),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) =>
+                    FadeTransition(opacity: animation, child: child),
+          ),
     ),
   ],
 );
