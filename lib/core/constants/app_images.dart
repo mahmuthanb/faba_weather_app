7d7ed9c1848435c7f7cec0582b12f3f5c4f29591
Mahmuthan <mahmuthanb@gmail.com>
2025-04-20 23:35:07 +0300

    new loading animation

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -1,3 +1,4 @@
 class AppImages {
   static const String logo = 'assets/images/faba_logo.png';
+  static const String loading = 'assets/animations/loading_animation.json';
 }
--- lib/core/widgets/loading_widget.dart
+++ lib/core/widgets/loading_widget.dart
@@ -1,3 +1,4 @@
+import 'package:faba_weather_app/core/constants/app_images.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -11,7 +12,7 @@ class LoadingWidget extends StatelessWidget {
       color: Colors.black54,
       child: Center(
         child: Lottie.asset(
-          'assets/animations/scattered_clouds.json',
+          AppImages.loading,
           width: size.width * 0.75,
           height: size.height * 0.75,
         ),
